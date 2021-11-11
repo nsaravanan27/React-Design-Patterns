6d6276c8a68cbf3ca84ea62928e668586b328050
sn <saravanan.n@radisys.com>
2021-11-11 19:55:58 +0530

    Model layout

--- react-resign-patterns/src/App.js
+++ react-resign-patterns/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import { ListContainer } from './Layout/Lists/ListContainer';
+import { LargePersonListItem } from './Layout/Lists/people/LargePersonListItem';
+import { people } from './Layout/Lists/SampleData';
+import { Modal } from './Layout/Model/Model';
 import { SplitScreen } from './Layout/SplitScreen';
 
 const LeftComponent = ({ name }) => (
@@ -13,9 +16,10 @@ const RightComponent = ({message}) => <p style={{ backgroundColor: 'yellow'}}> R
 function App() {
   return (
     <div className="App">
+      <Modal><LargePersonListItem person={people[0]}/></Modal>
       <SplitScreen leftWeight={1} rightWeight={3}>
-        <LeftComponent name= 'name' />
-        <RightComponent message = 'message' />
+        <LeftComponent name="name" />
+        <RightComponent message="message" />
       </SplitScreen>
     </div>
   );
