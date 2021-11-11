3e26258ffef5924ed42d460163e97f76c7c1afc8
sn <saravanan.n@radisys.com>
2021-11-11 17:44:48 +0530

    Splitscreen, passing the left and right components as props.

--- react-resign-patterns/src/App.js
+++ react-resign-patterns/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { SplitScreen } from './Layout/SplitScreen';
+
+const LeftComponent = () => <h1 style={{ backgroundColor: 'gray'}}> Left! </h1>;
+const RightComponent = () => <p style={{ backgroundColor: 'yellow'}}> Right! </p>
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <SplitScreen left={LeftComponent} right={RightComponent} leftWeight={1} rightWeight={3} />
     </div>
   );
 }
---
+++ react-resign-patterns/src/Layout/SplitScreen/SplitScreen.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Pane = styled.div`
+  flex: ${props => props.weight}; 
+`;
+
+export const SplitScreen = ({ left: Left, right: Right, leftWeight = 1, rightWeight = 1  }) => {
+  console.log(leftWeight, rightWeight);
+  return (
+    <Container>
+      <Pane weight={leftWeight}>
+        <Left />
+      </Pane>
+      <Pane weight={rightWeight} >
+        <Right />
+      </Pane>
+    </Container>
+  );
+};
